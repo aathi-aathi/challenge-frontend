2c1bac1057b2ffc46256c89244f2221d4d0bdd11
aathi-aathi <aathivdr2004@gmail.com>
2024-08-22 07:40:17 +0530

    incompleted functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 
 import { useEffect, useState } from 'react'
 import './App.css'
-import { editData, getData } from './api'
+import { editData, getData} from './api'
 import img from './assets/completed.png'
-
+import crossImg from './assets/cross-image.png'
+import undoImg from './assets/undo.png'
 function App() {
 const [items,setItems]= useState([])
 const [count,setCount] = useState(0)
@@ -12,7 +13,15 @@ const getBox = async() =>{
  setItems(data)
 }
 const editBox = async(id)=>{
-  await editData(id)
+  await editData(id,{status:'completed'})
+  setCount(count+1)
+}
+const editNo = async(id)=>{
+  await editData(id,{status:'not completed'})
+  setCount(count+1)
+}
+const undo = async(id)=>{
+  await editData(id,{status:'incompleted'})
   setCount(count+1)
 }
 useEffect(()=>{
@@ -24,12 +33,26 @@ useEffect(()=>{
     <div className='containter'>
       {items.map((item)=>
     <div className='box' key={item.id}>
-      <p style={{color:'green',fontWeight:'bold'}}>{item.date}</p>
-     <h2 style={{color: item.isCompleted && 'green'}}>DAY {item.id}</h2>
-     {item.isCompleted && <img src={img}></img>}
-     {item.isCompleted? <p style={{color:'green'}}>I have done it</p> : 
-     <p><span>Are you completed ? 
-      </span><span style={{color:'green',cursor:'pointer'}} onClick={()=>editBox(item.id)}>Yes</span>/<span style={{color:'red'}}>No</span></p>}
+      <div className='date-img'>
+        {item.isCompleted=='completed' && <img className='undo-img' onClick={()=>undo(item.id)} src={undoImg}/>}
+        {item.isCompleted=='not completed' && <img className='undo-img' onClick={()=>undo(item.id)} src={undoImg}/>}
+      {item.isCompleted == 'completed' ? <p style={{color:'green',fontWeight:'bold'}}>{item.date}</p> :
+      <p style={{color:'red',fontWeight:'bold'}}>{item.date}</p>}
+      </div>
+      
+      <h2 style={{color: item.isCompleted == 'completed' ? 'green' :
+         item.isCompleted == 'not completed' ? 'red' : 'grey' }}>DAY {item.id}</h2>
+      {item.isCompleted=='not completed' && <img src={crossImg}></img>}
+      {item.isCompleted=='completed' && <img src={img}></img>}
+     
+     {item.isCompleted == 'completed' ? <p style={{color:'green'}}>I have done it</p> 
+     : item.isCompleted == 'not completed' ? <p style={{color:'red'}}>I haven't done it</p> 
+    : <p><span>Are you completed ? 
+    </span><span style={{color:'green',cursor:'pointer'}} onClick={()=>editBox(item.id)}>Yes</span>/
+    <span style={{color:'red'}} onClick={()=>editNo(item.id)}>No</span></p>
+    } 
+     
+    
     </div>
    )}
     </div>
--- src/api.js
+++ src/api.js
@@ -1,11 +1,13 @@
 const backendUrl = 'https://challege-backend.onrender.com'
+// const backendUrl = 'http://localhost:7303'
 const getData = async() =>{
     const response = await fetch(`${backendUrl}/challenge`)
     return await response.json()
 }
-const editData = async(id)=>{
+const editData = async(id,userData)=>{
     const response = await fetch(`${backendUrl}/edit/${id}`,{
         method:'POST',
+        body: JSON.stringify(userData),
         headers:{
             "Content-Type":"application/json; charset=utf-8"
         }
