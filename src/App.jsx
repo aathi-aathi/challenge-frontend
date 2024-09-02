115c8d8d82d17b7b19de9fd61ffbfb39a59df869
aathi-aathi <aathivdr2004@gmail.com>
2024-09-02 09:02:45 +0530

    date editting added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import { useEffect, useState } from 'react'
 import './App.css'
-import { editData, getData} from './api'
+import { changeEditData, editData, getData, setDateData} from './api'
 import img from './assets/completed.png'
 import crossImg from './assets/cross-image.png'
 import undoImg from './assets/undo.png'
@@ -9,6 +9,16 @@ function App() {
 const [items,setItems]= useState([])
 const [count,setCount] = useState(0)
 const [itemCount,setItemCount]=useState(0)
+const [date,setDate] =useState('')
+const handleChange= async(e)=>{
+  const inputDate = e.target.value;
+  const id = e.target.id
+  const [year,month,day]=inputDate.split('-')
+  const formattedDate = `${day}/${month}/${year}`
+  setDate(inputDate)
+  await setDateData(id,{date:formattedDate})
+  setCount(count+1)
+}
 const getBox = async() =>{
  const data = await getData()
  setItems(data)
@@ -27,6 +37,10 @@ const undo = async(id)=>{
   await editData(id,{status:'incompleted'})
   setCount(count+1)
 }
+const changeEditMode = async(id)=>{
+  await changeEditData(id)
+  setCount(count+1)
+}
 useEffect(()=>{
   getBox()
 },[count])
@@ -36,33 +50,31 @@ useEffect(()=>{
     <h2>{itemCount}/90</h2>
     <div className='containter'>
       {items.map((item)=>
-    <div className='box' key={item.id}>
-      <div className='date-img'>
-        {item.isCompleted=='completed' && <img className='undo-img' onClick={()=>undo(item.id)} src={undoImg}/>}
-        {item.isCompleted=='not completed' && <img className='undo-img' onClick={()=>undo(item.id)} src={undoImg}/>}
-      {item.isCompleted == 'completed' ? <p style={{color:'green',fontWeight:'bold'}}>{item.date}</p> :
-      <p style={{color:'red',fontWeight:'bold'}}>{item.date}</p>}
-      </div>
+        <div className='box' key={item.id}>
+          <div className='date-img'>
+            {item.isCompleted=='completed' && <img className='undo-img' onClick={()=>undo(item.id)} src={undoImg}/>}
+            {item.isCompleted=='not completed' && <img className='undo-img' onClick={()=>undo(item.id)} src={undoImg}/>}
+            {item.isCompleted == 'completed' ? <p style={{color:'green',fontWeight:'bold'}}>{item.date}</p> :
+            <p style={{color:'red',fontWeight:'bold'}}>{item.date}</p>}
+            {!item.isEdit ? <button style={{padding:'3px'}} onClick={()=>changeEditMode(item.id)}>edit</button>:<input type='date' className='date-input'  id={item.id} onChange={handleChange} value={date}/>}
+          </div>
       
-      <h2 style={{color: item.isCompleted == 'completed' ? 'green' :
-         item.isCompleted == 'not completed' ? 'red' : 'grey' }}>DAY {item.id}</h2>
-      {item.isCompleted=='not completed' && <img src={crossImg}></img>}
-      {item.isCompleted=='completed' && <img src={img}></img>}
+          <h2 style={{color: item.isCompleted == 'completed' ? 'green' :
+          item.isCompleted == 'not completed' ? 'red' : 'grey' }}>DAY {item.id}</h2>
+          {item.isCompleted=='not completed' && <img src={crossImg}></img>}
+          {item.isCompleted=='completed' && <img src={img}></img>}
      
-     {item.isCompleted == 'completed' ? <p style={{color:'green'}}>I have done it</p> 
-     : item.isCompleted == 'not completed' ? <p style={{color:'red'}}>I haven't done it</p> 
-    : <p><span>Are you completed ? 
-    </span><span style={{color:'green',cursor:'pointer'}} onClick={()=>editBox(item.id)}>Yes</span>/
-    <span style={{color:'red'}} onClick={()=>editNo(item.id)}>No</span></p>
-    } 
-     
-    
-    </div>
-   )}
+           {item.isCompleted == 'completed' ? <p style={{color:'green'}}>I have done it</p> 
+             : item.isCompleted == 'not completed' ? <p style={{color:'red'}}>I haven't done it</p> 
+              : <p><span>Are you completed ? 
+            </span><span style={{color:'green',cursor:'pointer'}} onClick={()=>editBox(item.id)}>Yes</span>/
+            <span style={{color:'red'}} onClick={()=>editNo(item.id)}>No</span></p>} 
+        </div>
+      )}
     </div>
     
    
-    </>
+  </>
   )
 }
 
--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-const backendUrl = 'https://challege-backend.onrender.com'
-// const backendUrl = 'http://localhost:7303'
+// const backendUrl = 'https://challege-backend.onrender.com'
+const backendUrl = 'http://localhost:7303'
 const getData = async() =>{
     const response = await fetch(`${backendUrl}/challenge`)
     return await response.json()
@@ -14,4 +14,23 @@ const editData = async(id,userData)=>{
     })
     return await response.json()
 }
-export {getData,editData}
+const changeEditData = async(id)=>{
+    const response = await fetch(`${backendUrl}/set-edit/${id}`,{
+        method:'PUT',
+        headers:{
+            "Content-Type":"application/json; charset=utf-8"
+        }
+    })
+    return await response.json()
+}
+const setDateData = async(id,userData)=>{
+    const response = await fetch(`${backendUrl}/change-date/${id}`,{
+        method:'POST',
+        body: JSON.stringify(userData),
+        headers:{
+            "Content-Type":"application/json; charset=utf-8"
+        }
+    })
+    return await response.json()
+}
+export {getData,editData,changeEditData,setDateData}
