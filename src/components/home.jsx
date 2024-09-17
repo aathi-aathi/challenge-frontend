0989167cb1663ad0d2a32c9ca236b5ae37339c43
aathi-aathi <aathivdr2004@gmail.com>
2024-09-17 10:14:56 +0530

    some styling added

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -32,23 +32,21 @@ setCount(count+1)
 useEffect(()=>{
     getActions()
 },[count])
-// if(loading){
-//     return (
-//     <div className="h-screen w-full flex justify-center items-center">
-//         <img className='h-16 w-16'src="https://www.icegif.com/wp-content/uploads/2023/10/icegif-293.gif"/>
-//     </div>
+if(loading){
+    return (
+    <div className="h-screen w-full flex justify-center items-center">
+        <img className='h-16 w-16'src="https://www.icegif.com/wp-content/uploads/2023/10/icegif-293.gif"/>
+    </div>
     
-// )}
+)}
     return(
         <div className="h-screen w-full  flex flex-col items-center  gap-1  ">
         <img className="mt-4 mb-2" src="https://cdn-icons-png.flaticon.com/128/17705/17705487.png"/>
-        <h1 className="font-bold text-xl text-pink-700">Happy Birth Day</h1>
-        <h2 className="font-bold text-3xl text-sky-400">My Cute Pondatii</h2>
         <div className="flex flex-col gap-4 items-center w-full">
-        {records.map((record)=> (<div key={record.id} className="flex gap-2 flex-col items-center border border-pink-400 rounded h-fit w-11/12 shadow-md max-w-96 ">
+        {records.map((record)=> (<div key={record.id} className="flex gap-2 flex-col items-center border border-sky-400 rounded h-fit w-11/12 shadow-md max-w-96 ">
         <div className="flex gap-4 justify-evenly items-center w-full mt-2">
         {record.isCompleted =='completed' ?<i
-         onClick={()=>unCheckFunc(record.id)} className='fa-solid fa-circle-check fa-2xl text-pink-500'></i>
+         onClick={()=>unCheckFunc(record.id)} className='fa-solid fa-circle-check fa-2xl text-sky-400'></i>
         : <i className='fa-regular fa-circle fa-2xl text-pink-500'  onClick={()=>checkFunc(record.id)}></i>}
         <h1 className="font-bold text-pink-600 text-4xl">D<span className="text-sky-400">A</span>Y-{record.id}</h1>
        {record.isEdit ?
